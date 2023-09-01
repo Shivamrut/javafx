c23431db677782eaf9ac894e3b74d4b4c7f154b1
maxus_red <incineroaraditya@gmail.com>
2023-09-01 19:29:05 +0530

    javafx 05

--- src/main/java/com/example/demo/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/com/example/demo/HelloApplication.java
@@ -173,6 +173,36 @@ package com.example.demo;
 //}
 
 //04
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+//
+//public class HelloApplication extends Application {
+//
+//    @Override
+//    public void start(Stage stage)  {
+//        try
+//        {
+//            Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+//
+//            stage.setScene(new Scene(root));
+//            stage.setResizable(false);
+//            stage.show();
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//}
+
+//05
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -180,21 +210,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
-
     @Override
-    public void start(Stage stage)  {
-        try
-        {
-            Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+    public void start(Stage stage) {
 
-            stage.setScene(new Scene(root));
-            stage.setResizable(false);
+        try{
+            //Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+            Scene scene = new Scene(root);
+            String css = this.getClass().getResource("application.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            stage.setScene(scene);
             stage.show();
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/com/example/demo/HelloController.java
@@ -1,36 +1,38 @@
 package com.example.demo;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.shape.Circle;
-
-
 //04
-public class HelloController {
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
+//import javafx.scene.shape.Circle;
+//
+//
 
-    @FXML
-    private Circle circle;
-    private double x,y;
+//public class HelloController {
+//
+//    @FXML
+//    private Circle circle;
+//    private double x,y;
+//
+//    public HelloController()
+//    {
+//
+//    }
+//
+//    public void up(ActionEvent e)
+//    {
+//        circle.setCenterY(y-=10);
+//    }
+//    public void down(ActionEvent e)
+//    {
+//        circle.setCenterY(y+=10);
+//    }
+//    public void left(ActionEvent e)
+//    {
+//        circle.setCenterX(x-=10);
+//    }
+//    public void right(ActionEvent e)
+//    {
+//        circle.setCenterX(x+=10);
+//    }
+//}
 
-    public HelloController()
-    {
-
-    }
-
-    public void up(ActionEvent e)
-    {
-        circle.setCenterY(y-=10);
-    }
-    public void down(ActionEvent e)
-    {
-        circle.setCenterY(y+=10);
-    }
-    public void left(ActionEvent e)
-    {
-        circle.setCenterX(x-=10);
-    }
-    public void right(ActionEvent e)
-    {
-        circle.setCenterX(x+=10);
-    }
-}
